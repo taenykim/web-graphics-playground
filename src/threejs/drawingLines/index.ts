0cc22443c5b71d1b3abfd9f40e126d3bd2d8b5ad
taenykim <kimtaeeeny@gmail.com>
2022-05-05 11:07:14 +0900

    feat: 선 그리기

--- src/threejs/drawingLines/index.ts
+++ src/threejs/drawingLines/index.ts
@@ -1,30 +1,30 @@
 import * as THREE from "three";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  1,
+  500
+);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
-camera.position.z = 5;
+const scene = new THREE.Scene();
 
-function animate() {
-  requestAnimationFrame(animate);
+const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
 
-  renderer.render(scene, camera);
-}
-animate();
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+const line = new THREE.Line(geometry, material);
+
+scene.add(line);
+renderer.render(scene, camera);
