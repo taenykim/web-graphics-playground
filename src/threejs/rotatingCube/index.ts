69b40004e9b2a0f7d7dc015d2ae6be217aa075c8
taenykim <kimtaeeeny@gmail.com>
2022-05-07 06:36:09 +0900

    feat: 회전하는 큐브 2개 더 추가

--- src/threejs/rotatingCube/index.ts
+++ src/threejs/rotatingCube/index.ts
@@ -13,9 +13,27 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+
+function makeInstance(
+  geometry: THREE.BoxGeometry,
+  color: string | number,
+  x: number
+) {
+  const material = new THREE.MeshPhongMaterial({ color });
+
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  cube.position.x = x;
+
+  return cube;
+}
+
+const cubes = [
+  makeInstance(geometry, 0x44aa88, 0),
+  makeInstance(geometry, 0x8844aa, -2),
+  makeInstance(geometry, 0xaa8844, 2),
+];
 
 camera.position.z = 5;
 
@@ -25,12 +43,27 @@ const light = new THREE.DirectionalLight(color, intensity);
 light.position.set(-1, 2, 4);
 scene.add(light);
 
-function animate() {
-  requestAnimationFrame(animate);
+// function animate() {
+//   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+//   cube.rotation.x += 0.01;
+//   cube.rotation.y += 0.01;
 
+//   renderer.render(scene, camera);
+// }
+// animate();
+
+function render(time: number) {
+  time *= 0.001; // convert time to seconds
+
+  cubes.forEach((cube, ndx) => {
+    const speed = 1 + ndx * 0.1;
+    const rot = time * speed;
+    cube.rotation.x = rot;
+    cube.rotation.y = rot;
+  });
   renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
 }
-animate();
+requestAnimationFrame(render);
