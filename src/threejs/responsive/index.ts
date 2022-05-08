a2f966114845a0373d93ea7a4e01228b9344d1ca
taenykim <kimtaeeeny@gmail.com>
2022-05-08 12:53:07 +0900

    feat: HD-DPI 디스플레이 대응

--- src/threejs/responsive/index.ts
+++ src/threejs/responsive/index.ts
@@ -49,15 +49,15 @@ function main() {
 
   function resizeRendererToDisplaySize(renderer: THREE.WebGLRenderer) {
     const canvas = renderer.domElement;
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
+    const pixelRatio = window.devicePixelRatio;
+    const width = (canvas.clientWidth * pixelRatio) | 0;
+    const height = (canvas.clientHeight * pixelRatio) | 0;
     const needResize = canvas.width !== width || canvas.height !== height;
     if (needResize) {
       renderer.setSize(width, height, false);
     }
     return needResize;
   }
-
   function render(time: number) {
     time *= 0.001;
 
