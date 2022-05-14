b9b3b5627f2f346e867c2fabffe5e5b9e447d709
taenykim <kimtaeeeny@gmail.com>
2022-05-15 08:35:36 +0900

    add svg 애니메이션

--- src/animejs/index.ts
+++ src/animejs/index.ts
@@ -48,3 +48,13 @@ anime({
     logEl.innerHTML = JSON.stringify(battery);
   },
 });
+
+anime({
+  targets: ["polygon", "feTurbulence", "feDisplacementMap"],
+  points: "64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96",
+  baseFrequency: 0,
+  scale: 1,
+  loop: true,
+  direction: "alternate",
+  easing: "easeInOutExpo",
+});
