36a703ee597e228dc34fd0113a5b05d478c85b27
taenykim <kimtaeeeny@gmail.com>
2022-05-14 10:27:46 +0900

    feat: animejs

--- src/animejs/index.ts
+++ src/animejs/index.ts
@@ -1,18 +1,26 @@
 import anime from "animejs";
 
 let state = 0;
+let rotate = 0;
 
 const button = document.querySelector(".button");
 button?.addEventListener("click", () => {
+  rotate++;
   if (state === 0) {
     anime({
       targets: ".el",
-      translateX: 500,
+      translateX: 200,
+      borderRadius: ["0%", "25%"],
+      backgroundColor: ["#ff0000", "#00ff00"],
+      rotate: `${rotate}turn`,
     });
   } else if (state === 1) {
     anime({
       targets: ".el",
       translateX: 0,
+      borderRadius: ["25%", "0%"],
+      backgroundColor: ["#0000ff", "#ffff00"],
+      rotate: `${rotate}turn`,
     });
   }
   state = (state + 1) % 2;
@@ -34,8 +42,9 @@ anime({
   charged: 300,
   cycles: 130,
   round: 1,
+  안녕: 300,
   easing: "linear",
   update: function () {
-    logEl.innerHTML = JSON.stringify(battery.charged);
+    logEl.innerHTML = JSON.stringify(battery);
   },
 });
